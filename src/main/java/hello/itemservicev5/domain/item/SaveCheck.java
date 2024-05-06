55fbef9c687d8f200e0f29c0223b50a52d5355dc
geunhokinn <gghh1220@gmail.com>
2024-05-06 15:39:26 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/hello/itemservicev5/domain/item/SaveCheck.java
@@ -0,0 +1,4 @@
+package hello.itemservicev5.domain.item;
+
+public interface SaveCheck {
+}
---
+++ src/test/java/hello/itemservicev5/ItemServiceV5ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservicev5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceV5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
