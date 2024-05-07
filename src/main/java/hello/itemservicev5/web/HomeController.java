0090bf583ae8209ad20c4fef7ca9b6c3e7ada9ec
geunhokinn <gghh1220@gmail.com>
2024-05-08 01:17:38 +0900

    feat: @SessionAttribute 로 세션 조회 기능 추가

--- src/main/java/hello/itemservicev5/web/HomeController.java
+++ src/main/java/hello/itemservicev5/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -57,15 +58,33 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
+        // 세션이 있는지 조회
         HttpSession session = request.getSession(false);
         if (session == null) {
             return "home";
         }
 
+        // SessionConst.LOGIN_MEMBER 세션 이름이 있는지 조회
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV4(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+        // 세션 조회 체크, SessionConst.LOGIN_MEMBER 세션이 있는지 체크 모두 해줌.
+        // required : 값이 없을 수도 있음
 
         // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
